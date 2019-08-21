157623a5371c777914364eba09fdba369d0d6b0d
A-Kija <42836152+A-Kija@users.noreply.github.com>
2019-08-21 12:45:20 +0300

    Pirmas

---
+++ failas.php
@@ -0,0 +1,22 @@
+<h1>Labas</h1>
+
+
+<?php
+$trecias = 5;
+echo ++$trecias;
+echo $trecias;
+
+echo '<br>';
+
+$pirmas = 58888;
+$antras = '7ku kū';
+@$trecias = $pirmas . $antras;
+
+echo $trecias;
+
+echo '<br>';
+
+$pirmas = 'bla bla';
+$antras = 'ku'. $pirmas .'kū';
+
+echo $antras;
